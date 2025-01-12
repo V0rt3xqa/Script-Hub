96fca7d034f3af0c3b85e59036103ec9b5fe4e51
gggggggeaijnbghaalasd <125642781+gggggggeaijnbghaalasd@users.noreply.github.com>
2025-01-11 23:02:25 -0600

    lucky block/deobf destruction sim

---
+++ Games/LuckyBlock.lua
@@ -0,0 +1,33 @@
+local Mercury = loadstring(game:HttpGet("https://raw.githubusercontent.com/deeeity/mercury-lib/master/src.lua"))()
+local GUI = Mercury:Create{
+    Name = "Mercury",
+    Size = UDim2.fromOffset(600, 400),
+    Theme = Mercury.Themes.Dark,
+    Link = "https://github.com/deeeity/mercury-lib"
+}
+local Tab = GUI:Tab{
+	Name = "New Tab",
+	Icon = "rbxassetid://8569322835"
+}
+local block = nil
+
+Tab:Button{
+	Name = "Spawn Lucky Block",
+	Description = nil,
+	Callback = function() 
+        game:GetService("ReplicatedStorage"):WaitForChild(block):FireServer()
+    end
+}
+local MyDropdown = Tab:Dropdown{
+	Name = "LuckyBlock Type",
+	StartingText = "Select...",
+	Description = nil,
+	Items = {
+		{"Normal", "SpawnLuckyBlock"}, 		
+		{"Super", "SpawnSuperBlock"},		
+        {"Rainbow", "SpawnRainbowBlock"},
+        {"Diamond", "SpawnDiamondBlock"},
+        {"Galaxy", "SpawnGalaxyBlock"}		
+	},
+	Callback = function(item) block = item return end
+}
--- Loader.lua
+++ Loader.lua
@@ -1,7 +1,9 @@
 local gameid = game.GameId
 
 if gameid == 2248408710 then
-
+    loadstring(readfile('Games/DestructionSim.lua'))()
+elseif gameid == 662417684 then
+    loadstring(readfile('Games/LuckyBlock.lua'))()
 else
 
 end
